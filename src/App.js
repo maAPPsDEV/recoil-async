522c7f8db6fe83cc9c6211f138ed12453be31d6a
Tony Ma <tony@maapps.dev>
2021-04-01 08:38:46 -0400

    Error handling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {atom, RecoilRoot, selector, useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
 import {useState, Suspense} from 'react';
+import {ErrorBoundary} from "react-error-boundary";
 
 async function myDBQuery({userID}) {
   return new Promise(resolve => {
@@ -7,6 +8,7 @@ async function myDBQuery({userID}) {
       resolve({
         id: userID,
         name: `Name of ${userID}`,
+        error: userID < 0 ? 'There was an error while attempting to retrieve user info.' : undefined,
       });
     }, 2000);
   });
@@ -24,6 +26,11 @@ const currentUserNameQuery = selector({
     const response = await myDBQuery({
       userID: get(currentUserIDState)
     });
+
+    if (response.error) {
+      throw response.error;
+    }
+
     return response.name;
   },
 });
@@ -32,10 +39,14 @@ function App() {
   return (
     // @RECOIL: Wrap
     <RecoilRoot>
-      <Suspense fallback={<div>Loading...</div>}>
-        <CurrentUserIDInput />
-        <CurrentUserInfo />
-      </Suspense>
+      <ErrorBoundary
+          fallback={<div>Error!</div>}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <CurrentUserIDInput />
+          <CurrentUserInfo />
+        </Suspense>
+      </ErrorBoundary>
     </RecoilRoot>
   );
 }
