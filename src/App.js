1ff09f71a095ae1c7024d527065e6c54a1bfa9d9
Tony Ma <tony@maapps.dev>
2021-04-01 08:48:09 -0400

    Query with Parameter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {atom, RecoilRoot, selector, useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
+import {atom, RecoilRoot, selector, selectorFamily, useRecoilState, useRecoilValue, useSetRecoilState} from 'recoil';
 import {useState, Suspense} from 'react';
 import {ErrorBoundary} from "react-error-boundary";
 
+// A fake async function that returns results in 2 seconds.
+// It returns an error result when the userID is less than zero.
 async function myDBQuery({userID}) {
   return new Promise(resolve => {
     setTimeout(() => {
@@ -19,7 +21,7 @@ const currentUserIDState = atom({
   key: 'currentUserIDState',
 });
 
-// @RECOIL: Selector
+// @RECOIL: Async Selector a.k.a Query
 const currentUserNameQuery = selector({
   key: 'CurrentUserName',
   get: async ({get}) => {
@@ -35,14 +37,28 @@ const currentUserNameQuery = selector({
   },
 });
 
+// @RECOIL: Query with Parameter
+const userNameQuery = selectorFamily({
+  key: 'UserName',
+  get: userID => async () => {
+    const response = await myDBQuery({userID});
+    if (response.error) {
+      throw response.error;
+    }
+
+    return response.name;
+  },
+})
+
 function App() {
   return (
     // @RECOIL: Wrap
     <RecoilRoot>
-      <ErrorBoundary
-          fallback={<div>Error!</div>}
-      >
+      <ErrorBoundary fallback={<div>Error!</div>}>
         <Suspense fallback={<div>Loading...</div>}>
+          <UserInfo userID={1} />
+          <UserInfo userID={2} />
+          <UserInfo userID={3} />
           <CurrentUserIDInput />
           <CurrentUserInfo />
         </Suspense>
@@ -53,6 +69,11 @@ function App() {
 
 function CurrentUserInfo() {
   const userName = useRecoilValue(currentUserNameQuery);
+  return <div>{userName}</div>
+}
+
+function UserInfo({userID}) {
+  const userName = useRecoilValue(userNameQuery(userID));
   return <div>{userName}</div>
 }
 
